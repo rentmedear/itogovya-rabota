43d9d88ded0e175812cb7e649e10694244c1325a
lain <fgjhfgjf@gmail.com>
2024-02-28 18:59:18 +0300

    завершил работу, выполнил пункт №3

--- kontrolnaya.cs
+++ kontrolnaya.cs
@@ -15,4 +15,19 @@ string[] Zapros_array = (Zapros.Split(","));
 return Zapros_array;
 }
 
-
+string[] Len_array()
+{
+string[] array = Zapros();
+string new_string = String.Empty;
+for(int count = 0;count<array.Length;count++)
+{
+if(array[count].Length <= 3)
+{
+new_string=new_string+array[count]+",";
+}
+}
+new_string = new_string.Remove(new_string.Length-1);
+string[] new_array = new_string.Split(",");
+return new_array;
+}
+PrintArray(Len_array());
