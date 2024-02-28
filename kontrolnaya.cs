7427aa0d4c77db2bfa947bb97a416175f6211a80
lain <fgjhfgjf@gmail.com>
2024-02-28 18:53:36 +0300

    выполнил часть работы(№1,№2)

--- kontrolnaya.cs
+++ kontrolnaya.cs
@@ -1,1 +1,18 @@
-﻿
+﻿void PrintArray(string[] array)
+{
+for(int c = 0;c < array.Length; c++)
+{
+Console.Write(array[c]);
+Console.Write(", ");
+}
+}
+
+string[] Zapros ()
+{
+Console.WriteLine("Введите значения через запятую в одну строку: ");
+string Zapros = Console.ReadLine();
+string[] Zapros_array = (Zapros.Split(","));
+return Zapros_array;
+}
+
+
